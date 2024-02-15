7f3b209dd003dd0e51b7b9e6007ac1265764a416
chiayunliu0010 <136723973+chiayunliu0010@users.noreply.github.com>
2024-02-15 02:09:58 +0000

    First version completed

--- gyro-attempt-1/public/javascript/sketch.js
+++ gyro-attempt-1/public/javascript/sketch.js
@@ -1,5 +1,3 @@
-
-
 // Create connection to Node.JS Server
 const socket = io();
 
@@ -17,6 +15,10 @@ let rocket, mat;
 let averageAmt = 0.1;
 let curArgs = [];
 
+let stars = [];
+let numberOfStars = 2000; // Total number of stars
+let cloudVector = [];
+
 function preload() {
   rocket = loadModel('./javascript/assets/Rocket.obj', true);
   mat = loadImage('./javascript/assets/texture.png');
@@ -25,52 +27,115 @@ function preload() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   //debugMode();
+  //camera(0, 0, 250);
   createEasyCam();
   for(let i = 0; i < 3; i++) {
     curArgs[i] = 0;
+  }
+
+  for(let j = 0; j < 4; j++) {
+    cloudVector[j] = [];
+  }
+
+  let starFieldSize = 2000; 
+
+  for (let i = 0; i < numberOfStars; i++) {
+    stars.push({
+      x: random(-starFieldSize, starFieldSize), // Using the starFieldSize variable to increase the range
+      y: random(-starFieldSize, starFieldSize),
+      z: random(-starFieldSize, starFieldSize)
+    });
   }
   
 }
 
 function draw() {
-  background(200);
+  background(20);
 
   noStroke();
   lights();
 
+  drawStars(2000);
+  rocketMovement();
+}
+
+function rocketMovement(){
   push();
 
   rotateZ(roll);
   rotateX(pitch);
   rotateY(yaw);
 
-  translate(xAxis, yAxis, zAxis);
+  translate(0, yAxis, zAxis);
   texture(mat);
   model(rocket);
 
   pop();
 
+  let cXPos = [];
+  let cYPos = [];
+  let cZPos = [];
+
+  cXPos[0] = 50*cos(yaw) + 30;
+  cYPos[0] = 50*cos(pitch) + yAxis;
+  cZPos[0] = 50*sin(pitch) + 50*sin(yaw) + zAxis;
+
+  cXPos[1] = 50*cos(yaw) + 30;
+  cYPos[1] = -50*cos(pitch) + yAxis;
+  cZPos[1] = -50*sin(pitch) + 50*sin(yaw) + zAxis;
+
+  /*for(let i = 0; i < cXPos.length; i++) {
+    push();
+    fill(255, 200, 200);
+    translate(cXPos[i], cYPos[i], cZPos[i]);
+    sphere(10, 10, 10);
+    pop();
+  }*/
+
+  for(let i = 0; i < cXPos.length; i++) {
+    let pos = createVector(cXPos[i], cYPos[i], cZPos[i]); 
+    cloudVector[i].push(pos); //document the position of trail vertex
+
+    if(cloudVector[i].length > 50) {
+      cloudVector[i].splice(0, 1);
+    }
+  }
+
+  for(let i = 0; i < cloudVector.length; i++) {
+    push();
+
+    for(let j = 0; j < cloudVector[i].length; j++) {
+
+      let colour = map(j, 50, 0, 255, 0);
+      strokeWeight(5);
+      stroke(255, colour);
+      noFill();
+      if(j > 0) {
+        beginShape();
+        vertex(cloudVector[i][j].x + 500-(10*j), cloudVector[i][j].y, cloudVector[i][j].z);
+        vertex(cloudVector[i][j-1].x + 500-(10*(j+1)), cloudVector[i][j-1].y, cloudVector[i][j-1].z);
+        endShape();
+      }
+      
+    }
+
+    pop();
+  }
+}
+
+function drawStars(numberOfStars) {
+  push(); // Save the current drawing settings
+  stroke(255); // Set the colour of the star to white
+  strokeWeight(2); // You can adjust the size of the stars
+  for (let star of stars) {
+    point(star.x, star.y, star.z);
+  }
+  pop(); // Restore the previous drawing settings
 }
 
 //process the incoming OSC message and use them for our sketch
 function unpackOSC(message){
 
-  /*-------------
-
-  This sketch is set up to work with the gryosc app on the apple store.
-  Use either the gyro OR the rrate to see the two different behaviors
-  TASK: 
-  Change the gyro address to whatever OSC app you are using to send data via OSC
-  ---------------*/
-
-  //maps phone rotation directly 
-  // if(message.address == "/gyrosc/gyro"){
-  //   roll = message.args[0]; 
-  //   pitch = message.args[1];
-  //   yaw = message.args[2];
-  // }
-
-  //uses the rotation rate to keep rotating in a certain direction
   if(message.address == "/gyrosc/gyro"){
     roll = message.args[0] * 0.5;
     pitch = message.args[1] * 0.5;
@@ -87,10 +152,8 @@ function unpackOSC(message){
     }
 
     xAxis = curArgs[0] * 500;
-    yAxis = curArgs[1] * 500;
+    yAxis = -curArgs[1] * 500;
     zAxis = curArgs[2] * 500;
-
-    console.log(xAxis);
   }
 }
 
